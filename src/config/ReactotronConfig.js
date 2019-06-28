014a38fa76b46c0ba330059b443d5153f32efd6f
Élvio Gabriel Gravata <eggravata@hotmail.com>
2019-06-28 16:56:16 -0300

    Put an StyleSheet

--- src/config/ReactotronConfig.js
+++ src/config/ReactotronConfig.js
@@ -1,6 +1,6 @@
 import Reactotron from 'reactotron-react-native';
 
 console.tron = Reactotron
-.configure({host: '192.168.191.101', port: 9090})
+.configure()
 .useReactNative()
 .connect()
--- src/pages/main.js
+++ src/pages/main.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react'
-import { View, Text, TextInput, Button, FlatList, CheckBox } from 'react-native'
+import { View, Text, TextInput, Button, FlatList, CheckBox, StyleSheet } from 'react-native'
 import moment from 'moment'
 
 export default class Main extends Component {
   static navigationOptions = {
-    headerTitleStyle: { textAlign: 'center' },
+    headerTitleStyle: { alignSelf: 'center' },
     title: "ToDo List",
   }
 
@@ -41,22 +41,8 @@ export default class Main extends Component {
         endDate: index === itemsSelected ? status === true ? moment() : item.endDate : item.endDate,
       })),
     });
-    console.tron.log(items)
+    //console.tron.log(items)
   }
-
-  // renderItem = ({ item, index }) => {
-  //   return (
-  //     <View>
-  //       <CheckBox value={false} onChange={(e) => this.handleUpdate(index, e.target.checked)}/>
-  //       <Text style={{ textDecorationLine: item.done === true ? 'line-through' : "" }}>
-  //         {item.title + '\nData de inicio: ' 
-  //         + moment(item.startDate).format("dddd, MMMM Do YYYY, h:mm:ss a") 
-  //         + '\nData de término: ' 
-  //         + moment(item.endDate).format("dddd, MMMM Do YYYY, h:mm:ss a")}
-  //       </Text>
-  //     </View>
-  //   )
-  // }
 
   render() {
     const { items, newItem } = this.state;
@@ -79,15 +65,24 @@ export default class Main extends Component {
             extraData={this.state}
             renderItem = {({ item, index }) => {
               return (
-                <View>
-                  <CheckBox value={item.done} onChange={() => this.handleUpdate(index, !item.done)}/>
-                  <Text style={{ textDecorationLine: item.done === true ? 'line-through' : "none" }}>
-                    {item.title + '\nData de inicio: ' 
-                    + moment(item.startDate).format("dddd, MMMM Do YYYY, h:mm:ss a") 
-                    + '\nData de término: ' 
-                    + moment(item.endDate).format("dddd, MMMM Do YYYY, h:mm:ss a")}
-                  </Text>
-                  <Text>{JSON.stringify(item, 2, 2)}</Text>
+                <View style={styles.content}>
+                  <View style={styles.contentMainView}>
+                    <CheckBox value={item.done} onChange={() => this.handleUpdate(index, !item.done)}/>
+                    <Text style={{ textDecorationLine: item.done === true ? 'line-through' : "none", ...styles.title }}>
+                      {item.title}
+                    </Text>
+                  </View>
+                  <View style={styles.contentSecondView}>
+                    <Text style={{ textDecorationLine: item.done === true ? 'line-through' : "none", ...styles.dates }}>
+                      {'Data de inicio: ' 
+                      + moment(item.startDate).format("dddd, MMMM Do YYYY, h:mm:ss a")}
+                    </Text>
+                    <Text style={{ textDecorationLine: item.done === true ? 'line-through' : "none", ...styles.dates }}>
+                      {'Data de término: ' 
+                      + moment(item.endDate).format("dddd, MMMM Do YYYY, h:mm:ss a")}
+                    </Text>
+                    {/* <Text>{JSON.stringify(item, 2, 2)}</Text> */}
+                  </View>
                 </View>
               )
             }}
@@ -96,3 +91,34 @@ export default class Main extends Component {
     );
   }
 }
+
+const styles = StyleSheet.create({
+  content: {
+    borderRadius:10,
+    borderWidth: 1,
+    borderColor: '#000',
+    marginBottom: 10,
+    marginVertical: 10
+  },
+  contentMainView:{
+    flex:1/4,
+    flexDirection:'row',
+    justifyContent:'center',
+  },
+  contentSecondView:{
+    flex:3/4,
+    //marginTop: 10,
+    flexDirection:'column',
+    alignItems: 'stretch',
+    justifyContent:'center'
+  },
+  title: {
+    flex: 1,
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  dates: {
+    flex:1,
+    fontSize: 16,
+  }
+})
